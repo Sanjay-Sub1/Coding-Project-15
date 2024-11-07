f585fc438b09520698788bd4634d1a6093b13c35
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-11-06 22:14:40 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,23 @@
+import { assets } from './asset.js';
 
+function calculatePortfolioValue() {
+  return assets.reduce((total, asset) => {
+    return total + (asset.price * asset.quantity);
+  }, 0);
+}
+
+function getPortfolioAllocation() {
+  const totalValue = calculatePortfolioValue();
+  
+  return assets.map(asset => {
+    const assetValue = asset.price * asset.quantity;
+    const allocation = ((assetValue / totalValue) * 100);
+    return {
+      id: asset.id,
+      name: asset.name,
+      type: asset.type,
+      allocation: `${allocation}%`
+    };
+  });
+}
+export { calculatePortfolioValue, getPortfolioAllocation };
